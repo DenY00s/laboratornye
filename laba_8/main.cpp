e30973f02ef2c7a5f1e79e2f9a4339472298ebe0
DenY00s <noviko8d@mail.ru>
2022-11-25 12:30:08 +0200

    Добавлена база проекта 8

---
+++ laba_8/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main() {
+	// переменная типа long long
+	long long value = LLONG_MAX;
+
+	// размер long long: 8 байт
+	for (int i = 0; i < 8; i++)
+	{
+		// приводим значение к указателю unsigned char, чтобы получить указатель на первый байт
+		unsigned char* uc_ptr = (unsigned char*) &value;
+
+		// перемещаем указатель вперёд на количество итераций
+		uc_ptr += i;
+
+		// производим разыименование указателя и приведение символьного значения к целому
+		int byteValue = (int) *uc_ptr;
+
+		// выводим полученное значение
+		std::cout << "byte " << i + 1 << ": " << byteValue << std::endl;
+	}
+}
