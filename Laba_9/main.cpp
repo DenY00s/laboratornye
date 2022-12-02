42a6c888fbe2d0c927fba065e4a1d9482fdf9a4c
DenY00s <noviko8d@mail.ru>
2022-12-02 12:03:06 +0200

    Добавлена база проекта 9

---
+++ Laba_9/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+class Circle {
+private:
+    unsigned int m_radius;
+public:
+    Circle(double radius) : m_radius(radius) {}
+
+    double getRadius() {
+        return m_radius;
+    }
+
+    double getLength() {
+        return 2 * acos(-1) * m_radius;
+    }
+};
+
+int main() {
+    Circle circle(1);
+
+    std::cout << circle.getLength() << std::endl;
+}
