465a7adee81a043763fb1789b530ce6b303042de
DenY00s <noviko8d@mail.ru>
2023-03-18 12:13:49 +0200

    Изменена база 3 лабораторной

--- laba_3_2_semestr/build/cmake_install.cmake
+++ laba_3_2_semestr/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/projects/laba_3
+# Install script for directory: D:/projects/laba_2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/projects/laba_3/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/projects/laba_2/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- laba_3_2_semestr/main.cpp
+++ laba_3_2_semestr/main.cpp
@@ -1,43 +1,119 @@
-#include <iostream>
-#include <fstream>
-//1.
-void f()
-{
-	static int count = 0; // static, heap, static
-	count++;
-	std::cout << count << std::endl;
-}
-//2.
-class Log
+﻿#include <iostream>
+
+class Complex
 {
 private:
-	static std::ofstream m_out;
-
+	double m_a;
+	double m_b;
+	
 public:
-	Log()
+
+	
+	explicit Complex(double a = 0.0, double b = 0.0) :m_a(a), m_b(b)
 	{
-		std::cout << "Constr" << std::endl;
+		std::cout << "Basic constructor" << std::endl;
 	}
-	static void SETLOGPATH(const std::string& path)
+	Complex(const Complex& z)
 	{
-		m_out.open(path);
+		std::cout << "Copy constructor" << std::endl;
+		m_a = z.m_a;
+		m_b = z.m_b;
 	}
-	static void Write(const std::string& msg)
+	Complex& operator=(Complex z)
 	{
-		std::cout << msg << std::endl;
-		m_out << msg;
+		std::cout << "Copy assigment operator" << std::endl;
+		std::swap(m_a, z.m_a);
+		std::swap(m_b, z.m_b);
+		return *this;
 	}
-	~Log()
+	Complex& operator+=(const Complex& z)
 	{
-		std::cout << "Destr" << std::endl;
+		(*this).m_a += z.m_a;
+		(*this).m_b += z.m_b;
+		return *this;
 	}
+
+	Complex operator+(const Complex& z)
+	{
+		Complex temp = *this;
+		temp += z;
+		return temp;
+
+	}
+	Complex& operator*=(const Complex& z)
+	{
+		(*this).m_a = ((*this).m_a*z.m_a)-((*this).m_b * z.m_b);
+		(*this).m_b = ((*this).m_a * z.m_b) + ((*this).m_b * z.m_a);
+		return *this;
+	}
+
+	Complex operator*(const Complex& z)
+	{
+		Complex temp = *this;
+		temp *= z;
+		return temp;
+
+	}
+
+	
+	Complex& operator++()
+	{
+		this->m_a += 1;
+		return *this;
+	}
+	// постфиксный
+	Complex& operator++(int)
+	{
+		Complex temp = *this;
+		this->m_a += 1;
+		return temp;
+	}
+	friend std::ostream& operator<<(std::ostream& out, const Complex& z);
+	friend std::istream& operator>>(std::istream& in, Complex& z);
+	~Complex() = default;
+	
 };
-std::ofstream Log::m_out;
+
+Complex operator+(const Complex& z1, const Complex& z2) // rvo
+{
+	Complex temp = z1;
+	temp += z2;
+	return temp;
+
+}
+Complex operator*(const Complex& z1, const Complex& z2) // rvo
+{
+	Complex temp = z1;
+	temp *= z2;
+	return temp;
+
+}
+
+std::ostream& operator<<(std::ostream& out, const Complex& z)
+{
+	out << z.m_a << " " << z.m_b << "i";
+	return out;
+}
+std::istream& operator>>(std::istream& in, Complex& z)
+{
+	in >> z.m_a >> z.m_b;
+	return in;
+}
 int main()
 {
-	Log::SETLOGPATH("log.txt");
-	Log::Write("Hello,world!");
+	Complex z1(1);
+	Complex z2;
+	std::cin >> z1;
+	std::cin >> z2;
+	std::cout << z1;
+	std::cout << z2;
 
+	std::cout << (z1 * z2);
+	Complex z3(2, 5);
+	std::cout << z3;
 
 	return 0;
 }
+
+
+
